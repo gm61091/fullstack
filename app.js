d8513eb5c6622015ab6dbbc163dfe7ae00aa94e8
Dane <housejanssen@gmail.com>
2022-04-07 12:38:07 -0400

    lined up back end to make images display correctly and added index js file and configured app js file

--- app.js
+++ app.js
@@ -1,1 +1,21 @@
 //starter file
+
+
+const express = require('express');
+const app = express();
+
+const port = 3000;
+app.use(express.static('public'));
+
+app.set('view engine', 'ejs');
+
+//body parser to scrape info from header
+app.use(express.urlencoded({extended: false}));
+app.use(express.json());
+
+//routes
+app.use(require('./routes/index'));
+
+app.listen(port, ()=>{
+    console.log(`listening on port ${port}`);
+})
