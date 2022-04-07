71cc57046d0c94a5074d83878f1a1a751b3061bd
Dane <housejanssen@gmail.com>
2022-04-07 15:21:21 -0400

    converted html file routes for ABOUT1 CART and CHECKOUT, removed social media from footers

--- app.js
+++ app.js
@@ -4,7 +4,7 @@
 const express = require('express');
 const app = express();
 
-const port = 3000;
+const port = 3009;
 app.use(express.static('public'));
 
 app.set('view engine', 'ejs');
@@ -15,6 +15,17 @@ app.use(express.json());
 
 //routes
 app.use(require('./routes/index'));
+app.use(require('./routes/about1'));
+app.use(require('./routes/404'));
+app.use(require('./routes/cart'));
+app.use(require('./routes/menu2'));
+app.use(require('./routes/product-listing'));
+app.use(require('./routes/product'));
+app.use(require('./routes/single-product'));
+app.use(require('./routes/checkout'));
+app.use(require('./routes/contact-w-sidebar'));
+
+
 
 app.listen(port, ()=>{
     console.log(`listening on port ${port}`);
--- routes/single-product.js
+++ routes/single-product.js
@@ -2,8 +2,8 @@
 const express = require("express");
 const router = express.Router();
 
-router.get("/", (req, res) => {
-    res.render("index");
+router.get("/single-product", (req, res) => {
+    res.render("single-product");
 });
 
 
