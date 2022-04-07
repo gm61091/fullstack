274697b7368e4cdd979785f555b5e2412eda8af0
gary merriman <g.merriman691@gmail.com>
2022-04-07 15:23:44 -0400

    save

--- init.bash
+++ init.bash
@@ -1,5 +1,7 @@
 
 # sequelize model:generate --name products --attributes name:string,type:string,price:float,description:string,imgurl:string
 # sequelize model:generate --name users --attributes firstName:string,lastName:string,email:string,password:string
-sequelize model:generate --name orders --attributes price:float,userID:integer
+# sequelize model:generate --name orders --attributes price:float,userID:integer
+
+
 
